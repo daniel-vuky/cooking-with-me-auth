6697a3aa166600ca16c76b9ec0dfc97fe23126a2
Daniel <vukyanh001@gmail.com>
2024-02-02 13:56:09 +0700

    add morgan log

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connectoDb = require("./config/database");
 const errorHandler = require("./middleware/errorHandler");
+const morgan = require("morgan");
+const fs = require('fs');
+const path = require("path");
 
 const app = express();
 const port = process.env.SERVER_PORT || 8080;
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log/auth.log'), { flags: 'a' });
 
 connectoDb();
+
 app.use(express.json());
+app.use(morgan("tiny", { stream: accessLogStream }));
 app.use("/users/", require("./routes/userRoutes"));
 app.use(errorHandler);
 
